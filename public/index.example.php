2e5a668e4ff49b92b56bb9b9631b311a94c89ca5
Elivandro <Elivandro@hotmail.com.br>
2026-06-28 14:55:06 -0400

    style: apply Pint code style to usage example

--- public/index.example.php
+++ public/index.example.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 // ---------------------------------------------------------------------------
 // Standalone usage
@@ -8,13 +8,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use RetroAchievements\Services\RetroAchievementsApiClient;
 
-$api = new RetroAchievementsApiClient();
+$api = new RetroAchievementsApiClient;
 $api->setApiKey('your-api-key-here');
 
-$profile                   = $api->getUserProfile('MaxMilyin');
+$profile = $api->getUserProfile('MaxMilyin');
 $achievementsEarnedBetween = $api->getAchievementsEarnedBetween('MaxMilyin', '2024-01-01', '2024-01-30');
-$userSummary               = $api->getUserSummary('MaxMilyin', 1, 3);
-$consoleIds                = $api->getConsoleIDs();
+$userSummary = $api->getUserSummary('MaxMilyin', 1, 3);
+$consoleIds = $api->getConsoleIDs();
 
 var_dump($profile);
 var_dump($achievementsEarnedBetween);
@@ -29,14 +29,14 @@ use Illuminate\Support\Facades\Facade;
 use RetroAchievements\Facades\RetroAchievements;
 use RetroAchievements\Providers\RetroAchievementsProvider;
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
 $dotenv->load();
 
-$app = new Application(__DIR__ . '/../');
+$app = new Application(__DIR__.'/../');
 
 $app->singleton('config', function () {
     return new Repository([
-        'retroachievements' => require __DIR__ . '/../src/config/retroachievements.php',
+        'retroachievements' => require __DIR__.'/../src/config/retroachievements.php',
     ]);
 });
 
@@ -54,7 +54,7 @@ $provider->boot();
 
 RetroAchievements::setApiKey('your-api-key-here'); // optional if set in .env
 
-$profile      = RetroAchievements::getUserProfile('MaxMilyin');
+$profile = RetroAchievements::getUserProfile('MaxMilyin');
 $achievements = RetroAchievements::getUserRecentAchievements('MaxMilyin', 525600);
 
 var_dump($profile);
