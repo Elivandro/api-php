59564d6709875de9d6bc0392afb5577262614c2e
Elivandro <Elivandro@hotmail.com.br>
2026-06-28 14:38:56 -0400

    feat: add custom exceptions for API error handling

---
+++ src/Exceptions/InvalidDateException.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RetroAchievements\Exceptions;
+
+class InvalidDateException extends RetroAchievementsException {}
---
+++ src/Exceptions/RetroAchievementsException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RetroAchievements\Exceptions;
+
+use RuntimeException;
+
+class RetroAchievementsException extends RuntimeException {}
